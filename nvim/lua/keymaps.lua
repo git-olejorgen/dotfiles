38bd3461cccd7768934c5f3e2bfbbf720ae17d57
git-olejorgen <git@olejorgen.com>
2025-07-13 16:12:49 +0200

    updated nvim keybinds

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -10,16 +10,19 @@ vim.keymap.set("n", "<leader>t", ":tabnew<CR>")
 vim.keymap.set("n", "<leader>r", ":tabnext<CR>")
 vim.keymap.set("n", "<leader>tq", ":tabc<CR>")
 
--- Clipboard operations
-vim.keymap.set("n", "<leader>c", '"+y', { noremap = true, silent = true})
-vim.keymap.set("v", "<leader>c", '"+y', { noremap = true, silent = true})
 
 -- LSP keymaps
 vim.keymap.set("n", "<leader>k", vim.lsp.buf.hover, {})
 vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, {})
+vim.keymap.set("n", "<leader>)", vim.diagnostic.goto_next, {})
+vim.keymap.set("n", "<leader>(", vim.diagnostic.goto_prev, {})
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, {})
+vim.keymap.set("n", "<leader>d", function()
+  vim.diagnostic.enable(not vim.diagnostic.is_enabled())
+end, { desc = "Toggle diagnostics"})
+
 
 -- File explorer (Neo-tree)
 vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
